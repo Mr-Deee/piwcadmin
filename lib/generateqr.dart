4ddbdbd89de4a8b49d19e767c9411cadd86f78a0
MR.NARTERH <merchantdaniel8@gmail.com>
2023-09-21 23:54:53 +0000

    get attendance

--- lib/generateqr.dart
+++ lib/generateqr.dart
@@ -101,7 +101,7 @@ class _AttendanceQRCodeScreenState extends State<AttendanceQRCodeScreen> {
                       );
                     },
                   ),
-      );
+
                   ],)
 
               ],
