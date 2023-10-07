6b1e523485c642073017d5fc06dafc2f389dbcfc
MR.NARTERH <merchantdaniel8@gmail.com>
2023-10-07 15:47:23 +0000

    signup signin

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:piwcadmin/pages/signin.dart';
+import 'package:piwcadmin/pages/signup.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'generateqr.dart';
+import 'pages/generateqr.dart';
 import 'package:firebase_core/firebase_core.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,7 +13,8 @@ void main() async {
   );
   runApp(const MyApp());
 }
-
+DatabaseReference clients = FirebaseDatabase.instance.ref().child("Clients");
+DatabaseReference Admin = FirebaseDatabase.instance.ref().child("Admin");
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -19,27 +22,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PIWC_ADMIN',
+      title: 'PIWCADMIN',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  AttendanceQRCodeScreen(),
+      home:  Signin(),
+
+    initialRoute: FirebaseAuth.instance.currentUser == null
+    ? "/login"
+        :"/generateqr",
+    // : Doctorspage.idScreen,
+
+    routes: {
+    "/login": (context) => Signin(),
+    "/generateqr":(context)=>AttendanceQRCodeScreen(),
+    "/registration": (context) => SignUp(),
+
+
+
+  }
+
     );
   }
 }
---
+++ lib/pages/signin.dart
@@ -0,0 +1,360 @@
+import 'package:flutter/cupertino.dart';
+import 'dart:ui';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:piwcadmin/pages/signup.dart';
+
+import '../main.dart';
+import '../widgets/progressdialog.dart';
+import '../widgets/behavior.dart';
+
+class Signin extends StatefulWidget {
+  const Signin({super.key});
+
+  @override
+  State<Signin> createState() => _SigninState();
+}
+
+class _SigninState extends State<Signin> {
+  String? _email, _password;
+  TextEditingController email = new TextEditingController();
+  TextEditingController password = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    bool? isPassword;
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: ScrollConfiguration(
+        behavior: MyBehavior(),
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: size.height,
+            child: Stack(
+              children: [
+                SizedBox(
+                  height: size.height,
+                  child: Image.asset(
+                    'assets/images/background_image.jpg',
+                    // #Image Url: https://unsplash.com/photos/bOBM8CB4ZC4
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: SizedBox(),
+                      ),
+                      Expanded(
+                        flex: 7,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(30),
+                          child: BackdropFilter(
+                            filter: ImageFilter.blur(sigmaY: 100, sigmaX: 70),
+                            child: SizedBox(
+                              width: size.width * .9,
+                              child: SingleChildScrollView(
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.only(
+                                        top: size.width * .1,
+                                        bottom: size.width * .1,
+                                      ),
+                                      child: SizedBox(
+                                        height: 80,
+                                        child: Image.asset(
+                                          'assets/images/logo.png',
+                                          // #Image Url: https://unsplash.com/photos/bOBM8CB4ZC4
+                                          fit: BoxFit.fitHeight,
+                                        ),
+                                      ),
+                                    ),
+
+                                    //Username
+
+                                    //email
+
+                                    //Phone
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Container(
+                                        height: size.width / 8,
+                                        width: size.width / 1.25,
+                                        alignment: Alignment.center,
+                                        padding: EdgeInsets.only(
+                                            right: size.width / 30),
+                                        decoration: BoxDecoration(
+                                          color: Colors.black.withOpacity(.1),
+                                          borderRadius:
+                                              BorderRadius.circular(20),
+                                        ),
+                                        child: TextField(
+                                          style: TextStyle(
+                                            color: Colors.white.withOpacity(.9),
+                                          ),
+                                          controller: email,
+                                          // obscureText: isPassword,
+                                          keyboardType:
+                                              TextInputType.emailAddress,
+                                          decoration: InputDecoration(
+                                            prefixIcon: Icon(
+                                              Icons.email,
+                                              color:
+                                                  Colors.white.withOpacity(.8),
+                                            ),
+                                            border: InputBorder.none,
+                                            hintMaxLines: 1,
+                                            hintText: 'Email...',
+                                            hintStyle: TextStyle(
+                                              fontSize: 14,
+                                              color:
+                                                  Colors.white.withOpacity(.5),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Container(
+                                        height: size.width / 8,
+                                        width: size.width / 1.25,
+                                        alignment: Alignment.center,
+                                        padding: EdgeInsets.only(
+                                            right: size.width / 30),
+                                        decoration: BoxDecoration(
+                                          color: Colors.black.withOpacity(.1),
+                                          borderRadius:
+                                              BorderRadius.circular(20),
+                                        ),
+                                        child: TextField(
+                                          style: TextStyle(
+                                            color: Colors.white.withOpacity(.9),
+                                          ),
+                                          controller: password,
+                                          obscureText: true,
+                                          // keyboardType: isPassword ? TextInputType.name : TextInputType.text,
+                                          decoration: InputDecoration(
+                                            prefixIcon: Icon(
+                                              Icons.password,
+                                              color:
+                                                  Colors.white.withOpacity(.8),
+                                            ),
+                                            border: InputBorder.none,
+                                            hintMaxLines: 1,
+                                            hintText: 'Password...',
+                                            hintStyle: TextStyle(
+                                              fontSize: 14,
+                                              color:
+                                                  Colors.white.withOpacity(.5),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceAround,
+                                      children: [
+                                        RichText(
+                                          text: TextSpan(
+                                            text: 'Forgotten password!',
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                            ),
+                                            recognizer: TapGestureRecognizer()
+                                              ..onTap = () {
+                                                HapticFeedback.lightImpact();
+                                                Fluttertoast.showToast(
+                                                  msg:
+                                                      'Forgotten password! button pressed',
+                                                );
+                                              },
+                                          ),
+                                        ),
+                                        InkWell(
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(20),
+                                            child: Text(
+                                              'Create an account.',
+                                              style: TextStyle(
+                                                  color: Color(0xFFb1b2c4),
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                          ),
+                                          onTap: () {
+                                            Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) {
+                                                return SignUp();
+                                              }),
+                                            );
+                                          },
+                                        ),
+                                      ],
+                                    ),
+
+                                    SizedBox(height: size.width * .1),
+                                    InkWell(
+                                      splashColor: Colors.transparent,
+                                      highlightColor: Colors.transparent,
+                                      onTap: () {
+                                        loginAndAuthenticateUser(context);
+                                        HapticFeedback.lightImpact();
+                                        Fluttertoast.showToast(
+                                          msg: 'Sign-In button pressed',
+                                        );
+                                      },
+                                      child: Container(
+                                        margin: EdgeInsets.only(
+                                          bottom: size.width * .05,
+                                        ),
+                                        height: size.width / 8,
+                                        width: size.width / 1.25,
+                                        alignment: Alignment.center,
+                                        decoration: BoxDecoration(
+                                          color: Colors.black.withOpacity(.1),
+                                          borderRadius:
+                                              BorderRadius.circular(20),
+                                        ),
+                                        child: Text(
+                                          'Sign-In',
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: SizedBox(),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget component(IconData icon, String hintText, bool isPassword,
+      bool isEmail, TextEditingController controller) {
+    Size size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: size.width / 8,
+        width: size.width / 1.25,
+        alignment: Alignment.center,
+        padding: EdgeInsets.only(right: size.width / 30),
+        decoration: BoxDecoration(
+          color: Colors.black.withOpacity(.1),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: TextField(
+          style: TextStyle(
+            color: Colors.white.withOpacity(.9),
+          ),
+          controller: controller,
+          obscureText: isPassword,
+          keyboardType:
+              isEmail ? TextInputType.emailAddress : TextInputType.text,
+          decoration: InputDecoration(
+            prefixIcon: Icon(
+              icon,
+              color: Colors.white.withOpacity(.8),
+            ),
+            border: InputBorder.none,
+            hintMaxLines: 1,
+            hintText: hintText,
+            hintStyle: TextStyle(
+              fontSize: 14,
+              color: Colors.white.withOpacity(.5),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  void loginAndAuthenticateUser(BuildContext context) async {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return ProgressDialog(
+            message: "Logging you ,Please wait.",
+          );
+        });
+
+    Future signInWithEmailAndPassword(String email, String password) async {
+      try {
+        UserCredential result = await _firebaseAuth.signInWithEmailAndPassword(
+            email: email, password: password);
+        User? user = result.user;
+        return _firebaseAuth;
+      } catch (error) {
+        print(error.toString());
+        return null;
+      }
+    }
+
+    final User? firebaseUser = (await _firebaseAuth
+            .signInWithEmailAndPassword(
+                email: email.text.trim(), password: password.text.trim())
+            .catchError((errMsg) {
+      Navigator.pop(context);
+      displayToast("Error" + errMsg.toString(), context);
+    }))
+        .user;
+    try {
+      UserCredential userCredential =
+          await _firebaseAuth.signInWithEmailAndPassword(
+              email: email.text.trim(), password: password.text.trim());
+
+      if (email.text == "piwcmobile@gmail.com") {
+        Navigator.of(context).pushNamed("/generateqr");
+      } else if (Admin != null) {
+        Navigator.of(context).pushNamed("/generateqr");
+
+        displayToast("Logged-in ", context);
+      } else {
+        displayToast("Error: Cannot be signed in", context);
+      }
+    } catch (e) {
+      // handle error
+    }
+  }
+
+  //
+  displayToast(String message, BuildContext context) {
+    Fluttertoast.showToast(msg: message);
+
+// user created
+  }
+}
