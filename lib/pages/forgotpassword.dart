dafc17393ac62a719e8a6b8be79925ef6c42b31f
MR.NARTERH <merchantdaniel8@gmail.com>
2023-12-02 02:17:32 +0000

    forgotpassword

--- lib/pages/forgotpassword.dart
+++ lib/pages/forgotpassword.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/progressdialog.dart';
 class Forgotpassword extends StatefulWidget {
   const Forgotpassword({super.key});
 
@@ -13,15 +15,61 @@ class _ForgotpasswordState extends State<Forgotpassword> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   void _resetPassword() async {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return ProgressDialog(
+          message: "Resetting Password, Please wait.",
+        );
+      },
+    );
+
     try {
       await _auth.sendPasswordResetEmail(email: _emailController.text);
-      // Show a success message or navigate to another screen
+      // Password reset email sent successfully
       print("Password reset email sent successfully");
+
+      // Close the dialog
+      Navigator.of(context).pop();
+      Navigator.of(context).pop();
+
+      // You can show a success message or navigate to another screen here
+      // For example, show a snackbar:
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Password reset email sent successfully"),
+          duration: Duration(seconds: 2),
+        ),
+      );
     } catch (e) {
       // Handle errors, such as invalid email or user not found
       print("Error sending password reset email: $e");
+
+      // Close the dialog
+      Navigator.of(context).pop();
+
+      // Show an error message
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Error"),
+            content: Text("Error sending password reset email: $e"),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text("OK"),
+              ),
+            ],
+          );
+        },
+      );
     }
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +92,18 @@ class _ForgotpasswordState extends State<Forgotpassword> {
             SizedBox(height: 20),
             ElevatedButton(
 
-              onPressed: _resetPassword,
+              onPressed:(){
+                showDialog(
+                    context: context,
+                    barrierDismissible: false,
+                    builder: (BuildContext context) {
+                      return ProgressDialog(
+                        message: "Resetting Password ,Please wait.",
+                      );
+                    });
+
+                _resetPassword();
+                },
               child: Text("Reset Password"),
             ),
           ],
